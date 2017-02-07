f2d5cf8c6bb0e421ca5739d97c1397d6cc7c890e
armin langhofer <armin@langhofer.net>
2017-02-08 00:17:06 +0100

    temporarily allowing index() for app

--- src/Controller/HostsController.php
+++ src/Controller/HostsController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\ORM\TableRegistry;
 use Cake\Routing\Router;
+use Cake\Event\Event;
 
 class HostsController extends AppController {
     
