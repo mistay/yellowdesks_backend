822afd8a66ce12fe7f6a8333ebdbf9bb803ddc88
armin langhofer <armin@langhofer.net>
2017-03-10 17:38:12 +0100

    coworkers: register impl

--- src/Controller/CoworkersController.php
+++ src/Controller/CoworkersController.php
@@ -153,7 +153,7 @@ class CoworkersController extends AppController {
 
             $ret["success"] = true;
             $ret["coworker"] = [];
-            $ret["coworker"]["id"] = $ret["id"];
+            $ret["coworker"]["id"] = $row -> id;
         }
 
         echo json_encode($ret);
