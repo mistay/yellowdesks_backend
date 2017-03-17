21a3e9521138ec31b66f0525dc3e6f0927b89c4e
armin langhofer <armin@langhofer.net>
2017-03-17 17:15:57 +0100

    users: forgot passwd impl

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 use App\Controller\AppController;
 use Cake\Event\Event;
 use Cake\ORM\TableRegistry;
+use Cake\Mailer\Email;
+
 
 class UsersController extends AppController
 {
@@ -11,6 +13,68 @@ class UsersController extends AppController
     }
 
     public function forgotpassword() {
+        $modelCoworkers = TableRegistry::get('Coworkers');
+        $modelHosts = TableRegistry::get('Hosts');
+
+        if ($this -> request -> is('post')) {
+            $email = $this -> request -> data['email'];
+
+            if (strlen($email) < 3) {
+                $this -> Flash -> success (__("Please provide an E-Mail address with at least 3 characters."));
+                return;
+            }
+
+            $query = $modelCoworkers -> find('all') -> where(["Coworkers.email LIKE" => '%' . $email . '%']);
+            $coworkers = $query->toArray();
+
+            $query = $modelHosts -> find('all') -> where(["Hosts.email LIKE" => '%' . $email . '%']);
+            $hosts = $query->toArray();
+            
+            $num = (sizeof($coworkers) + sizeof($hosts));
+            if ($num < 1) {
+                $this -> Flash -> success (__("Sorry, no such E-Mail address found."));
+                return;
+            } elseif ($num > 1) {
+                $this -> Flash -> success (__("Sorry, more than one E-Mail address found. Please provide a unique E-Mail address."));
+                return;
+            } else {
+                // genau eine e-mail adresse gefunden
+
+                if (sizeof($coworkers) == 1)
+                    foreach ($coworkers as $row) {
+                        //echo "mail:" . $row -> email;
+                    }
+                if (sizeof($hosts) == 1)
+                    foreach ($hosts as $row) {
+                        //echo "mail:" . $row -> email;
+                    }
+
+                $to = $row["email"];
+
+                // Sample SMTP configuration.
+                Email::setConfigTransport('langhofer', [
+                    'host' => 'ssl://ssl.langhofer.at',
+                    'port' => 25,
+                    'username' => 'armin',
+                    'password' => 'phOOb4r*',
+                    'className' => 'Smtp',
+                    'tls' => true
+                ]);
+
+                $email = new Email();
+                $email->setTransport('langhofer');
+                $ret = $email
+                    ->setTemplate('default')
+                    ->setLayout('default')
+                    ->setEmailFormat('both')
+                    ->setTo('office@langhofer.at')
+                    ->setFrom('office@langhofer.at')
+                    ->send();
+
+                echo "foo"; var_dump($ret);
+
+            }
+        }
     }
     
     public function loginappfb() {
